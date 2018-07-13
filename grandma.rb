417428916785784f7ed20fffea099b2b9c1ebaa2
Jennifer Hong <jennifer.hong13@gmail.com>
2018-07-13 03:37:16 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(speak)
   if speak == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase && speak != "I LOVE YOU GRANDMA!"
+  elsif speak == speak.upcase && speak != "I LOVE YOU GRANDMA!"
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"

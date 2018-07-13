4d604f302d69a79857132d764916577beba14a09
Jennifer Hong <jennifer.hong13@gmail.com>
2018-07-13 03:31:12 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,5 @@
-def speak_to_grandma == "I LOVE YOU GRANDMA!"
+def speak_to_grandma
+  if speak_to_grandma == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
 elsif speak_to_grandma == "i love you Grandma"
   puts "HUH?! SPEAK UP, SONNY!"
